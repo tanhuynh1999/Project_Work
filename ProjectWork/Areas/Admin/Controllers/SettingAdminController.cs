dc25dcfba73c0ddf3aca0087355b744a1eff9bad
Tấn Thanh <tanthanh141214@gmail.com>
2020-11-18 13:43:57 +0700

    edit home verson-1.0 done

--- ProjectWork/Areas/Admin/Controllers/SettingAdminController.cs
+++ ProjectWork/Areas/Admin/Controllers/SettingAdminController.cs
@@ -19,7 +19,7 @@ namespace ProjectWork.Areas.Admin.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult EditHome([Bind(Include = "layout_id,ct_1,ct_2,ct_3,ct_4,ct_5,ct_6,ct_7,ct_8,ct_9,ct_10,ct_11,ct_12,ct_13,ct_14,ct_15,ct_16")] Layout layout)
+        public ActionResult EditHome([Bind(Include = "layout_id,ct_1,ct_2,ct_3,ct_4,ct_5,ct_6,ct_7,ct_8,ct_9,ct_10,ct_11,ct_12,ct_13,ct_14,ct_15,ct_16,ct_17,ct_18,ct_19,ct_20,ct_21,ct_22,ct_23,ct_24,ct_25")] Layout layout)
         {
             db.Entry(layout).State = EntityState.Modified;
             db.SaveChanges();
