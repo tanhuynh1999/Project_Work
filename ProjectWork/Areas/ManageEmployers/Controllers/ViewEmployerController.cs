dc0a08f3d911d2768a62d5b43ef26269919a98f5
tanhuynh1999 <54873015+tanhuynh1999@users.noreply.github.com>
2020-11-25 10:23:31 +0700

    Reg

---
+++ ProjectWork/Areas/ManageEmployers/Controllers/ViewEmployerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectWork.Areas.ManageEmployers.Controllers
+{
+    public class ViewEmployerController : Controller
+    {
+        // GET: ManageEmployers/ViewEmployer
+        public PartialViewResult Vali()
+        {
+            return PartialView();
+        }
+    }
+}
