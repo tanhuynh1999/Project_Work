e87c8f0c5754ff64435ea8738a53733382fffca7
Tấn Thanh <tanthanh141214@gmail.com>
2020-12-14 13:26:25 +0700

    admin

---
+++ ProjectWork/Areas/Admin/Controllers/FormsAdminController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectWork.Models;
+
+namespace ProjectWork.Areas.Admin.Controllers
+{
+    public class FormsAdminController : Controller
+    {
+        private DataWork_projectEntities db = new DataWork_projectEntities();
+
+        // GET: Admin/FormsAdmin
+        public ActionResult Index()
+        {
+            return View(db.Forms.ToList());
+        }
+
+        // GET: Admin/FormsAdmin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Form form = db.Forms.Find(id);
+            if (form == null)
+            {
+                return HttpNotFound();
+            }
+            return View(form);
+        }
+
+        // GET: Admin/FormsAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/FormsAdmin/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "form_id,form_name")] Form form)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Forms.Add(form);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(form);
+        }
+
+        // GET: Admin/FormsAdmin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Form form = db.Forms.Find(id);
+            if (form == null)
+            {
+                return HttpNotFound();
+            }
+            return View(form);
+        }
+
+        // POST: Admin/FormsAdmin/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "form_id,form_name")] Form form)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(form).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(form);
+        }
+
+        // GET: Admin/FormsAdmin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Form form = db.Forms.Find(id);
+            if (form == null)
+            {
+                return HttpNotFound();
+            }
+            return View(form);
+        }
+
+        // POST: Admin/FormsAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Form form = db.Forms.Find(id);
+            db.Forms.Remove(form);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProjectWork/Areas/Admin/Controllers/PositionsAdminController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectWork.Models;
+
+namespace ProjectWork.Areas.Admin.Controllers
+{
+    public class PositionsAdminController : Controller
+    {
+        private DataWork_projectEntities db = new DataWork_projectEntities();
+
+        // GET: Admin/PositionsAdmin
+        public ActionResult Index()
+        {
+            return View(db.Positions.ToList());
+        }
+
+        // GET: Admin/PositionsAdmin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Position position = db.Positions.Find(id);
+            if (position == null)
+            {
+                return HttpNotFound();
+            }
+            return View(position);
+        }
+
+        // GET: Admin/PositionsAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/PositionsAdmin/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "position_id,position_name,position_datacreate,position_dateupdate")] Position position)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Positions.Add(position);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(position);
+        }
+
+        // GET: Admin/PositionsAdmin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Position position = db.Positions.Find(id);
+            if (position == null)
+            {
+                return HttpNotFound();
+            }
+            return View(position);
+        }
+
+        // POST: Admin/PositionsAdmin/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "position_id,position_name,position_datacreate,position_dateupdate")] Position position)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(position).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(position);
+        }
+
+        // GET: Admin/PositionsAdmin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Position position = db.Positions.Find(id);
+            if (position == null)
+            {
+                return HttpNotFound();
+            }
+            return View(position);
+        }
+
+        // POST: Admin/PositionsAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Position position = db.Positions.Find(id);
+            db.Positions.Remove(position);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
